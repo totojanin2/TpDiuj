98c8acacc984ec4afad0462f600762ec252f8bd8
ulukacher <uri.lukacher@gmail.com>
2017-06-10 18:35:16 -0300

    precarga los indicadores. Faltaria hacer que cuando selecciones uno, te muestre su formula

--- TpIntegradorDiuj/Controllers/IndicadoresController.cs
+++ TpIntegradorDiuj/Controllers/IndicadoresController.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿
+
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
 using TpIntegradorDiuj.Models;
@@ -14,11 +14,30 @@ namespace TpIntegradorDiuj.Controllers
         // GET: Indicadores
         public ActionResult Index()
         {
+            ViewBag.ListIndicadores = DeserializarArchivoIndicadores().Select(x => new SelectListItem {
+                Text = x.Nombre,
+                Value = x.Id.ToString()
+            }).ToList();
+            
             return View();
+        }
+        public List<Indicador> DeserializarArchivoIndicadores()
+        {
+            string buf = System.IO.File.ReadAllText(Server.MapPath("~/App_Data/Archivos/") + "indicadores.json");
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            List<Indicador> listIndicadores = serializer.Deserialize<List<Indicador>>(buf);
+            return listIndicadores;
+
         }
         public ActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public JsonResult ObtenerFormulaDelIndicador(int idIndicador)
+        {
+            var indicador = DeserializarArchivoIndicadores().FirstOrDefault(x => x.Id == idIndicador);
+            return Json(new { Formula = indicador.Formula });
         }
         [HttpPost]
         public ActionResult Create(Indicador model)
